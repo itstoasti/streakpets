51a16acbfe481a4f03acc4ac1a114102bd3a37db
itstoasti <deanfields48@gmail.com>
2026-01-13 11:04:53 -0700

    Cleanup project and add promotional homepage

--- delete-old-trivia.sql
+++
@@ -1,7 +0,0 @@
--- Delete the old trivia game so you can start a new one
-DELETE FROM games
-WHERE game_type = 'trivia'
-AND couple_id = 'ab54ae5b-1700-446c-a4ce-12d3eecd3b38';
-
--- Verify it's deleted
-SELECT * FROM games WHERE game_type = 'trivia';
--- enable-games-realtime.sql
+++
@@ -1,8 +0,0 @@
--- Enable realtime for games table
--- Run this in Supabase SQL Editor
-
--- Enable realtime on the games table
-ALTER PUBLICATION supabase_realtime ADD TABLE games;
-
--- Verify it was added (optional - just for checking)
--- SELECT * FROM pg_publication_tables WHERE pubname = 'supabase_realtime';
